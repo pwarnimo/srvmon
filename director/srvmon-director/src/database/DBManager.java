7c5d52997dd08444016cac850922f412baff0194
Pol Warnimont <pwarnimo@gmail.com>
2015-04-23 16:44:41 +0200

    LIN01 CHK-08 : Create the files for the SRVMON DIRECTOR

---
+++ director/srvmon-director/src/database/DBManager.java
@@ -0,0 +1,43 @@
+/*
+ * Class       : DBManager
+ * Author(s)   : Pol Warnimont <pwarnimo@gmail.com>
+ * Create date : 2015-04-23
+ * Version     : 0.1
+ *
+ * Description : This class is used to exchange data with the SQL server.
+ *
+ * Changelog
+ * ---------
+ *  2015-04-23 : Created class.
+ *
+ * License information
+ * -------------------
+ *  Copyright (C) 2015  Pol Warnimont
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+
+package database;
+
+/**
+ *
+ * @author pwarnimo
+ */
+public class DBManager {
+    public DBManager() {
+        System.out.println("> DBManager class init...");
+        System.out.println("> DBManager class Ok.");
+    }
+}

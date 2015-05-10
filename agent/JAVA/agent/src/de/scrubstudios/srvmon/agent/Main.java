915606c6a6fab05a7a66b95a756a130d205a593a
Pol Warnimont <pwarnimo@gmail.com>
2015-05-10 03:10:38 +0200

    X201 CHK-61 : Agent - Worked on the java agent

--- agent/JAVA/agent/src/de/scrubstudios/srvmon/agent/Main.java
+++ agent/JAVA/agent/src/de/scrubstudios/srvmon/agent/Main.java
@@ -1,9 +1,92 @@
+/*
+ * File        : Main.java
+ * Author(s)   : Pol Warnimont
+ * Create date : 2015-05-05
+ * Version     : 1.0
+ * Description : This file is part of the SRVMON agent.
+ *               This class contains the main logic.
+ *
+ * Changelog
+ * ---------
+ *  2015-05-09 : Created class.
+ *
+ * License information
+ * -------------------
+ *  Copyright (C) 2015  Pol Warnimont
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.scrubstudios.srvmon.agent;
 
-public class Main {
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
+public class Main {
+	private static String postData(String xml) {
+		System.out.println("REQ = " + xml);
+		
+		URL url;
+        HttpURLConnection connection = null;
+        
+        try {
+			url = new URL("http://127.0.0.1/srvmon-server/");
+			
+			connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("POST");
+            connection.setRequestProperty("Content-Type", "text/xml");
+            
+            connection.setRequestProperty("Content-Length", Integer.toString(xml.getBytes().length));
+            connection.setRequestProperty("Content-Language", "en-US");
+
+            connection.setUseCaches(false);
+            connection.setDoInput(true);
+            connection.setDoOutput(true);
+            
+            DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+            wr.writeBytes(xml);
+            wr.flush();
+            wr.close();
+            
+            InputStream is = connection.getInputStream();
+            BufferedReader rd = new BufferedReader(new InputStreamReader(is));
+            String line;
+            StringBuffer response = new StringBuffer();
+            while ((line = rd.readLine()) != null) {
+                response.append(line);
+                response.append('\r');
+            }
+            rd.close();
+            
+            return response.toString().trim();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("SRVMON AGENT");
+		
+		XML xml0 = new XML();
+		
+		System.out.println(postData(xml0.getServicesXML(24)));
 	}
-
 }
--- agent/JAVA/agent/src/de/scrubstudios/srvmon/agent/Service.java
+++ agent/JAVA/agent/src/de/scrubstudios/srvmon/agent/Service.java
@@ -1,3 +1,33 @@
+/*
+ * File        : Service.java
+ * Author(s)   : Pol Warnimont
+ * Create date : 2015-05-05
+ * Version     : 1.0
+ * Description : This file is part of the SRVMON agent.
+ *               This class defines a service check.
+ *
+ * Changelog
+ * ---------
+ *  2015-05-09 : Created class.
+ *
+ * License information
+ * -------------------
+ *  Copyright (C) 2015  Pol Warnimont
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package de.scrubstudios.srvmon.agent;
 
 public class Service {

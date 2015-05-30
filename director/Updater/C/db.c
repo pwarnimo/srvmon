a80ebe46b9578b131fdff3c725dc761f3b46402d
Pol Warnimont <pwarnimo@gmail.com>
2015-05-31 00:51:44 +0200

    T400 CHK-101 : Director - Adding C updater

---
+++ director/Updater/C/db.c
@@ -0,0 +1,13 @@
+#include "db.h"
+
+void exitErr(MYSQL *con) {
+	return;
+}
+
+void connectDB(char *dbhost, char *dbuser, char *dbpass, char *dbname) {
+	return;
+}
+
+void getServersFromDB(struct Server *servers) {
+	return;
+}
